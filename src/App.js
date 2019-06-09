39da7666101315118fa25bd8a086e99f84994e79
Ivan <belzee92@gmail.com>
2019-06-08 20:26:17 -0400

    setup bootstrap and custom styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import React from 'react';
+import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-      ready
-    </div>
-  );
+  return <div className="App">ready</div>;
 }
 
 export default App;
